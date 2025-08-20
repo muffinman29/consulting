029fd2a28abecee7dc7540c66ca6775d4bb2b7db
Steven <steven.williams012083@gmail.com>
2025-08-20 10:05:36 -0500

    updated about; updated contact page; added testimonials;

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  
 }
